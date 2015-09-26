9d75603944e93b1ee2c762d7e150b66b6ac212cf
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-09-26 21:21:11 +0200

    [TP2] Added NuancierRVB's Storyboard

---
+++ TP2/NuancierRVB/NuancierRVB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NuancierRVB
+//
+//  Created by m2sar on 26/09/2015.
+//  Copyright (c) 2015 m2sar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TP2/NuancierRVB/NuancierRVB/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  NuancierRVB
+//
+//  Created by m2sar on 26/09/2015.
+//  Copyright (c) 2015 m2sar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UISwitch *mySwitch;
+@property (weak, nonatomic) IBOutlet UISlider *myBSlider;
+@property (weak, nonatomic) IBOutlet UISlider *myVSlider;
+@property (weak, nonatomic) IBOutlet UISlider *myRSlider;
+
+@end
+
