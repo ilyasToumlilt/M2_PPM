eabf7b239f607ec26df3523c0e75a71fb636dc45
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-09-28 15:24:58 +0200

    [TP2] Working version of the Nuancier (ObjC)

--- TP2/NuancierRVB/NuancierRVB/ViewController.h
+++ TP2/NuancierRVB/NuancierRVB/ViewController.h
@@ -11,9 +11,23 @@
 @interface ViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UISwitch *mySwitch;
+@property (weak, nonatomic) IBOutlet UILabel *myBLabel;
 @property (weak, nonatomic) IBOutlet UISlider *myBSlider;
+@property (weak, nonatomic) IBOutlet UILabel *myVLabel;
 @property (weak, nonatomic) IBOutlet UISlider *myVSlider;
+@property (weak, nonatomic) IBOutlet UILabel *myRLabel;
 @property (weak, nonatomic) IBOutlet UISlider *myRSlider;
+@property (weak, nonatomic) IBOutlet UIButton *myButtonPrec;
+@property (weak, nonatomic) IBOutlet UIButton *myButtonPenu;
+@property (weak, nonatomic) IBOutlet UIView *myActualColorView;
+
+
+- (IBAction)onClickPrecPenu:(UIButton *)sender;
+- (IBAction)onChangeSliders:(UISlider *)sender;
+- (IBAction)onClickSave:(UIButton *)sender;
+- (IBAction)onClickReset:(UIButton *)sender;
+- (IBAction)onChangeSwitch:(UISwitch *)sender;
+
 
 @end
 
--- TP2/NuancierRVB/NuancierRVB/ViewController.m
+++ TP2/NuancierRVB/NuancierRVB/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self switchSetup];
+    [self resetSetup];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +26,60 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)resetSetup {
+    _myRSlider.value = _myVSlider.value = _myBSlider.value =  (_mySwitch.isOn) ? 5 : 50;
+    [self updateAll];
+    _myButtonPrec.backgroundColor = _myButtonPenu.backgroundColor = _myActualColorView.backgroundColor;
+}
+
+- (void)switchSetup {
+    if( _mySwitch.isOn ) {
+        _myRSlider.value = (int)_myRSlider.value/10;
+        _myVSlider.value = (int)_myVSlider.value/10;
+        _myBSlider.value = (int)_myBSlider.value/10;
+        _myRSlider.maximumValue = _myVSlider.maximumValue = _myBSlider.maximumValue = 10;
+    } else {
+        _myRSlider.maximumValue = _myVSlider.maximumValue = _myBSlider.maximumValue = 100;
+        _myRSlider.value = _myRSlider.value*10;
+        _myVSlider.value = _myVSlider.value*10;
+        _myBSlider.value = _myBSlider.value*10;
+    }
+    [self updateAll];
+}
+
+- (void)updateAll {
+    _myActualColorView.backgroundColor = [UIColor colorWithRed:_myRSlider.value/((_mySwitch.isOn)?10:100) green:_myVSlider.value/((_mySwitch.isOn)?10:100) blue:_myBSlider.value/((_mySwitch.isOn)?10:100) alpha:1];
+    _myRLabel.text = [NSString stringWithFormat:@"R: %d%@%%", (int)_myRSlider.value, ((_mySwitch.isOn) ? @"0":@"")];
+    _myVLabel.text = [NSString stringWithFormat:@"V: %d%@%%", ((int)_myVSlider.value), ((_mySwitch.isOn) ? @"0":@"")];
+    _myBLabel.text = [NSString stringWithFormat:@"B: %d%@%%", ((int)_myBSlider.value), ((_mySwitch.isOn) ? @"0":@"")];
+}
+
+- (IBAction)onClickPrecPenu:(UIButton *)sender {
+    CGFloat red = 0.0, green = 0.0, blue = 0.0, alpha =0.0;
+    [sender.backgroundColor getRed:&red green:&green blue:&blue alpha:&alpha];
+
+    
+    _myRSlider.value = (_mySwitch.isOn) ? red*10 : red*100;
+    _myVSlider.value = (_mySwitch.isOn) ? green*10 : green*100;
+    _myBSlider.value = (_mySwitch.isOn) ? blue*10 : green*100;
+    
+    [self updateAll];
+}
+
+- (IBAction)onChangeSliders:(UISlider *)sender {
+    [self updateAll];
+}
+
+- (IBAction)onClickSave:(UIButton *)sender {
+    _myButtonPenu.backgroundColor = _myButtonPrec.backgroundColor;
+    _myButtonPrec.backgroundColor = _myActualColorView.backgroundColor;
+}
+
+- (IBAction)onClickReset:(UIButton *)sender {
+    [self resetSetup];
+}
+
+- (IBAction)onChangeSwitch:(UISwitch *)sender {
+    [self switchSetup];
+}
 @end
