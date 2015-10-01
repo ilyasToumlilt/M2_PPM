e1470d3b619d88007dc7414c6a562c029e1fb818
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-10-01 20:55:55 +0200

    [TP3] Quizz: Initialized Storyboard

---
+++ TP3/QuizzSwift/QuizzSwift/QuestionsReponses.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionsReponses.swift
+//  QuizzSwift
+//
+//  Created by m2sar on 01/10/2015.
+//  Copyright (c) 2015 m2sar. All rights reserved.
+//
+
+import Foundation
+
+class QuestionsReponses {
+    <#properties and methods#>
+}
---
+++ TP3/QuizzSwift/QuizzSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuizzSwift
+//
+//  Created by m2sar on 01/10/2015.
+//  Copyright (c) 2015 m2sar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
