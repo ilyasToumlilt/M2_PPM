59405c057432d23168c6916a66830d78b16bae63
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-10-03 20:00:17 +0200

    [TP3] Quizz:Added some cool stuff

--- TP3/QuizzSwift/QuizzSwift/QuestionsReponses.swift
+++ TP3/QuizzSwift/QuizzSwift/QuestionsReponses.swift
@@ -8,6 +8,37 @@
 
 import Foundation
 
+let EASY_LEVEL = 0;
+let HARD_LEVEL = 1;
+
 class QuestionsReponses {
-    <#properties and methods#>
+    
+    let QuizzStack:[(difficulty: Int, question: String, answer: String)] =
+        [(EASY_LEVEL, "Q.1.1", "R.1.1"),
+         (EASY_LEVEL, "Q.1.2", "R.1.2"),
+         (HARD_LEVEL, "Q.2.1", "R.2.1"),
+         (EASY_LEVEL, "Q.1.3", "R.1.3"),
+         (HARD_LEVEL, "Q.2.2", "R.2.2")];
+    
+    var RspCounter   = 0;
+    var stackCounter = 0;
+    
+    func getNextQuestion(Difficulty: Int) -> String {
+        for (var i=self.stackCounter+1; i != self.stackCounter; i=(i+1)%self.QuizzStack.count) {
+            if( QuizzStack[i].difficulty == Difficulty ) {
+                self.stackCounter = i;
+                return self.QuizzStack[self.stackCounter].question;
+            }
+        }
+        return "Erreur: pile de questions vide pour ce niveau !";
+    }
+    
+    func getAnswer() -> String {
+        RspCounter++;
+        return self.QuizzStack[self.stackCounter].answer;
+    }
+    
+    func getRspCounter() -> Int {
+        return self.RspCounter;
+    }
 }
--- TP3/QuizzSwift/QuizzSwift/ViewController.swift
+++ TP3/QuizzSwift/QuizzSwift/ViewController.swift
@@ -8,11 +8,24 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    var QR = QuestionsReponses();
+    
+    @IBOutlet weak var myQuestionTV: UITextView!
+    @IBOutlet weak var myAnswerTV: UITextView!
+    @IBOutlet weak var myRspCounterLabel: UILabel!
+    @IBOutlet weak var myBalaiseSwitch: UISwitch!
+    @IBOutlet weak var myNextButton: UIButton!
+    @IBOutlet weak var myPreviousButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.myAnswerTV.textColor = UIColor.greenColor();
+        self.onChangeBalaiseSwitch(self.myBalaiseSwitch);
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onClickReponse(sender: UIButton) {
+        self.myAnswerTV.text = self.QR.getAnswer();
+        self.myRspCounterLabel.text = "Réponses vues : \(self.QR.getRspCounter())";
+    }
 
+    @IBAction func onClickNext(sender: UIButton) {
+        self.myQuestionTV.text = self.QR.getNextQuestion((self.myBalaiseSwitch.on ? HARD_LEVEL : EASY_LEVEL));
+        self.myAnswerTV.text = "";
+    }
+    
+    @IBAction func onChangeBalaiseSwitch(sender: UISwitch) {
+        self.myQuestionTV.textColor = self.myBalaiseSwitch.on ? UIColor.redColor() : UIColor.purpleColor();
+        self.onClickNext(self.myNextButton);
+    }
+    
 }
-
