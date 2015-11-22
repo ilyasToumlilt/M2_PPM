dc1684cc01e5641051668f668dbc2c373b71b0e9
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-11-22 20:59:45 +0100

    [TP8] MesActivites: Initialized Single View App

---
+++ TP8/MesActivites/MesActivites/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MesActivites
+//
+//  Created by ilyas TOUMLILT on 22/11/2015.
+//  Copyright (c) 2015 ilyas TOUMLILT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TP8/MesActivites/MesActivites/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MesActivites
+//
+//  Created by ilyas TOUMLILT on 22/11/2015.
+//  Copyright (c) 2015 ilyas TOUMLILT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
