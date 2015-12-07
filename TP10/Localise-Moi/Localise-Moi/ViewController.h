03430a3691428afc54811207ff11a7748ff94305
ilyasToumlilt <toumlilt.ilyas@gmail.com>
2015-12-07 10:08:05 +0100

    [TP10] Localise-Moi: Initialized with app icons

---
+++ TP10/Localise-Moi/Localise-Moi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Localise-Moi
+//
+//  Created by m2sar on 07/12/2015.
+//  Copyright (c) 2015 Ilyas Toumlilt - Adrien Ferreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TP10/Localise-Moi/Localise-MoiTests/Localise_MoiTests.m
@@ -0,0 +1,40 @@
+//
+//  Localise_MoiTests.m
+//  Localise-MoiTests
+//
+//  Created by m2sar on 07/12/2015.
+//  Copyright (c) 2015 Ilyas Toumlilt - Adrien Ferreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Localise_MoiTests : XCTestCase
+
+@end
+
+@implementation Localise_MoiTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
